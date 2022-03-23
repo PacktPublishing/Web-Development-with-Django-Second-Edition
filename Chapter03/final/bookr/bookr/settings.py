c069e51cef0c52ed9b1744644fda354701b838c8
Bharath Chandra <bharathchandraks@gmail.com>
2022-03-23 23:42:41 +1100

    Chapter 3 final fixed templates settings

--- Chapter03/final/bookr/bookr/settings.py
+++ Chapter03/final/bookr/bookr/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'bookr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
