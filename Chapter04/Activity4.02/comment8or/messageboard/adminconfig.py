c1decb3bdf15c1eccf0fdba656ae9722936b1065
Chris Guest <chrisguest@gmail.com>
2023-02-23 23:23:12 +1100

    Activity 4.02

---
+++ Chapter04/Activity4.02/comment8or/messageboard/adminconfig.py
@@ -0,0 +1,4 @@
+from django.contrib.admin.apps import AdminConfig
+
+class MessageboardAdminConfig(AdminConfig):
+    default_site = 'admin.Comment8orAdminSite'
---
+++ Chapter04/Activity4.02/comment8or/messageboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MessageboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messageboard'
