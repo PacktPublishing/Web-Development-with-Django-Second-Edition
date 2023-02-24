223c79b37d9efefd58e766f84175ed16109bcb1a
Bharath Chandra <bharathchandraks@gmail.com>
2023-02-25 10:09:25 +1100

    Chapter2 corrections after TR review

--- Chapter02/Exercise2.16/exercise2.16.py
+++ Chapter02/Exercise2.16/exercise2.16.py
@@ -5,6 +5,6 @@ from reviews.models import Publisher
 publishers = [Publisher.objects.get(name='New Town Publisher'), Publisher.objects.get(name='Byron Bay Press')]
 
 publishers[0].website = "www.newsouthwalespublisher.com"
-publishers[2].website = "www.newsouthwalespublisher.com"
+publishers[1].website = "www.newsouthwalespublisher.com"
 
 Publisher.objects.bulk_update(publishers, ["website"])
--- Chapter02/Exercise2.17/exercise2.17.py
+++ Chapter02/Exercise2.17/exercise2.17.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from django.db.models import Q
 from reviews.models import Publisher
 
 Publisher.objects.filter(Q(name__startswith="New") | Q(name__startswith="Idea"))
