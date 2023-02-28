e5f46580589a1c13e782e1e17b42087ca24abcfe
Chris Guest <chrisguest@gmail.com>
2023-02-28 23:24:41 +1100

    Exercise4.04

--- Chapter04/Exercise4.04/bookr/reviews/admin.py
+++ Chapter04/Exercise4.04/bookr/reviews/admin.py
@@ -8,25 +8,24 @@ class BookAdmin(admin.ModelAdmin):
     date_hierarchy = 'publication_date'
     list_display = ('title', 'isbn13')
     list_filter = ('publisher', 'publication_date')
-    search_fields = ('title', 'isbn', 'publisher__name')
     @admin.display(
         ordering='isbn',
         description='ISBN-13',
         empty_value='-/-'
     )
     def isbn13(self, obj):
-        """ '9780316769174' => '978-0-31-676917-4' """
-        return "{}-{}-{}-{}-{}".format(obj.isbn[0:3], obj.isbn[3:4],
-                                       obj.isbn[4:6], obj.isbn[6:12],
-                                       obj.isbn[12:13])
+        """'9780316769174' => '978-0-31-676917-4'"""
+        return "{}-{}-{}-{}-{}".format(
+            obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13]
+        )
 
 
 def initialled_name(obj):
-    """ obj.first_names='Jerome David', obj.last_names='Salinger'
-        => 'Salinger, JD' """
-    initials = ''.join([name[0] for name in \
-                        obj.first_names.split(' ')])
+    """obj.first_names='Jerome David', obj.last_names='Salinger'
+    => 'Salinger, JD' """
+    initials = ''.join([name[0] for name in obj.first_names.split(' ')])
     return "{}, {}".format(obj.last_names, initials)
+
 
 class ContributorAdmin(admin.ModelAdmin):
     list_display = (initialled_name,)
--- Chapter04/SearchBarExcludingGrouping/bookr/reviews/admin.py
+++ Chapter04/SearchBarExcludingGrouping/bookr/reviews/admin.py
@@ -15,17 +15,16 @@ class BookAdmin(admin.ModelAdmin):
         empty_value='-/-'
     )
     def isbn13(self, obj):
-        """ '9780316769174' => '978-0-31-676917-4' """
-        return "{}-{}-{}-{}-{}".format(obj.isbn[0:3], obj.isbn[3:4],
-                                       obj.isbn[4:6], obj.isbn[6:12],
-                                       obj.isbn[12:13])
+        """'9780316769174' => '978-0-31-676917-4' """
+        return "{}-{}-{}-{}-{}".format(
+            obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13]
+        )
 
 
 def initialled_name(obj):
     """ obj.first_names='Jerome David', obj.last_names='Salinger'
         => 'Salinger, JD' """
-    initials = ''.join([name[0] for name in \
-                        obj.first_names.split(' ')])
+    initials = ''.join([name[0] for name in obj.first_names.split(' ')])
     return "{}, {}".format(obj.last_names, initials)
 
 
